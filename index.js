ff7e528510747e0720322883ebb66291fd602173
Max Zelarayan <max.z_@hotmail.com>
2023-01-20 12:29:35 -0300

    Some last cleanup

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const FlattenArray = (arr) => {
    if (arr instanceof Array) {
       if (arr.length === 0) {
-          return [];
+          return []
       } 
       if (arr.length > 0) { 
-          return FlattenArray(arr[0]).concat(FlattenArray(arr.slice(1)));
+          return FlattenArray(arr[0]).concat(FlattenArray(arr.slice(1)))
       } 
    }
 
-   return [arr];  
-};
+   return [arr]  
+}
 
 let arr = [
     1,
@@ -17,8 +17,8 @@ let arr = [
     [[3], [4]],
     [5],
     [[[6, 7, 8]]]
-];
-const res = FlattenArray(arr);
+]
+const res = FlattenArray(arr)
  
-// Result
+// Result: [ 1, 2, 3, 4, 5, 6, 7, 8 ]
 console.log('res', res)
