8a85f61dd3484081b893c622ffb958cb68dcb71d
Max Zelarayan <max.z_@hotmail.com>
2023-01-20 12:23:01 -0300

    Adding a function to flatten arrays

---
+++ index.js
@@ -0,0 +1,24 @@
+const FlattenArray = (arr) => {
+   if (arr instanceof Array) {
+      if (arr.length === 0) {
+          return [];
+      } 
+      if (arr.length > 0) { 
+          return FlattenArray(arr[0]).concat(FlattenArray(arr.slice(1)));
+      } 
+   }
+
+   return [arr];  
+};
+
+let arr = [
+    1,
+    [2],
+    [[3], [4]],
+    [5],
+    [[[6, 7, 8]]]
+];
+const res = FlattenArray(arr);
+ 
+// Result
+console.log('res', res)
